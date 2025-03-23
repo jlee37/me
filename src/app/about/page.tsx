530ac492561ce93d2b31be1b40035f26d637cbda
jlee37 <jonathanslee4@gmail.com>
2025-03-23 10:31:03 -0700

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function AboutPage() {
   return (
     <ContentPageWrapper>
-      <h1 className="text-xl md:text-2xl mb-2">Huh? What is this place?</h1>
+      <h1 className="text-xl md:text-2xl mb-2">All these little souls</h1>
       <p>
         {`The inspiration for this website is Neocities, a web
         hosting service that hosts more than a million hyper-stylized websites. Here are a few of my favorites:`}
@@ -26,107 +26,56 @@ export default function AboutPage() {
         </Link>
       </div>
       <p>
-        {`Each one, like you can probably tell, is a hyper-stylized little spot in the internet. They're self-expression; they're comfort. They're small pockets of community, even– visit a popular Neocity and chances are that you'll see a message board of some kind, a place where people have left all manner of warm notes for the creator of the space. 
+        {`Each one's a hyper-stylized little spot in the internet, a little oasis of self-expression. They're small pockets of community, even– visit a popular Neocity and chances are that you'll see a message board of some kind, a place where people have left notes for the creator of the space. 
   
-        And regardless of what the Neocity looks like, whether there's flashing gifs everywhere and not a name in sight, or a fully-fledged bio and a list of all the books and songs the person's ever loved, I feel like the same sentiment runs behind every
-        single site– behind every single one, someone's saying: "Hey, here I am!
+        Regardless of what the Neocity looks like, though, whether there's flashing gifs everywhere and not a name in sight, or a fully-fledged bio and a list of all the books and songs the person's ever wanted to love, the same sentiment runs behind every
+        single site– behind every single one, there's someone, someone out there in the hugeness of this world, saying: "Hey, here I am!
         Here this is!"`}
       </p>
       <br />
       <p>
-        {`In the beginning, though, it made me sad to think about how for every single Neocity, there'll come a time when it gets its last update. There's all sorts of reasons why something like that might happen, after all. The person behind it might get tired of keeping it up-to-date. Or they might pass away, or find some better way to express themselves. It doesn't even have to be that person– all it would take is for the people running the Neocities platform from the top to kick the bucket, and a million sites, a million custom cursors, a million heartfelt self-introductions would turn to 404 slush. So one wonders: is this all for
+        {`But what struck me as sad, when I first started visting all these sites, is how a lot of the creators write about how the reason they decided to put their Neocity together in the first place
+        is because of how permanent one is, permanent in a way that a journal
+        isn't, that the human body isn't. It's sad, because it's
+        not really true. There's no guarantee that the people running the Neocities service will still want to do it a year from now, five years from now. Sooner or later they'll get tired of it, or they'll pass away. Even if humanity invented
+        some sort of anti-aging device, there'd still be the
+        heat-death of the universe, so really there's no way, no how: Neocities
+        will end. A million sites, a million custom cursors, a million heartfelt
+        self-introductions will one day turn to error 404 slush. So one wonders: is this all for
         nothing! Is this how every Neocity will be remembered, as an amusement park ditched and left behind? 
         `}
       </p>
       <br />
       <p>
-        {`Well, obviously not. But I'm going to take a slight tangent before getting to why. *Flashback sounds* Ever since I was very little, I had this idea of
-        myself as being not one person, but instead just one version of countless
-        Jonny's. I still see myself this way; in every moment, I really am a different person, down to the atomic level– the me who spewed out this big blob of
-        text is a different me than the one who an hour ago hadn't even started
-        typing. The me who stubbed my toe a few days ago was a different person
-        even than the one who said "ow, fuck," half a second later. It's the same for everyone. Small things add up; one day you look in the mirror and you're older than you remember. You wake up to a you who isn't so sure she wants kids, after all. And
-        on and on, so that the very act of going through life is a constant kind of dying, and living, and dying again.`}
+        {`Well, obviously not. It's a so-true–it's–corny fact of life, after all, that everything ends. The me who spewed out this big blob of
+        text replaced the me who an hour ago hadn't even started
+        typing. Even the me who stubbed my toe earlier today is a different person
+        even than the one who said "ow, fuck," half a second later. And on and on, things are gained, things are lost, so that going through life, I think, is really just a constant kind of dying, and living again.`}
       </p>
-      So I guess what I'm saying, in a very roundabout way, is that things
-      "ending" isn't such an awful thing. We say bye to bits of ourselves
-      everyday, and really there's nothing hopeless or pointless about it; it's
-      the most natural thing in the world. Nothing is taken away from those
-      Neocities still-blinking, just as nothing is taken away from the ones that
-      have seemed. ! So, then, the point of nothing is taken away from any of
-      the million Neocities, just as nothing will be taken away from any of
-      their corpses.
       <br />
       <p>
-        {`So, then, the point of this website, its intended audience, isn't really "me." Instead, it's for you! You, reading this, wherever and
-        whenever you happen to be, whether you're a good friend of mine or a
-        future version of me looking back and cringing; this is all for you.
-        
-        So, then, the point of a website like this is to capture, hopefully over the course of a long time, a few different versions of "me." It's not meant to go on forever– like all those Neocities there will come a time when jonnylee.net will come crashing down. Maybe I'll have decided to take it down myself, or maybe I'll have been killed a Western shootout of some kind, who knows. 
-        
-        
-        
-        , Different things I've seen, thoughts I've had, people I've been. But then who's this website for? is maybe a good question to ask. And the answer is that it's for you! You, reading this, wherever and
-        whenever you happen to be, whether you're a good friend of mine or a
-        future version of me looking back and cringing; this is all for you! It
-        can't be for me, after all, because like I mentioned before the me who
-        wrote this message is already long gone. You'll have lived enough life
-        by the time you get here that this me, the one writing here on March 17,
-        2025, will be no more than a memory of a memory. Or if you're a friend,
-        it'll be a different Jonny you have in your life, a different version of
-        me. Maybe for whatever reason I won't be in your life anymore at all.
-        But that's the point of all this
-        
-        
-        
-        It'll be my own sort of Neocity. 
-        
-        So, then, what's the point of a website like this? who's this website for? That's an important question to ask.
-        The answer is easy: it's for you! You, reading this, wherever and
-        whenever you happen to be, whether you're a good friend of mine or a
-        future version of me looking back and cringing; this is all for you! It
-        can't be for me, after all, because like I mentioned before the me who
-        wrote this message is already long gone. You'll have lived enough life
-        by the time you get here that this me, the one writing here on March 17,
-        2025, will be no more than a memory of a memory. Or if you're a friend,
-        it'll be a different Jonny you have in your life, a different version of
-        me. Maybe for whatever reason I won't be in your life anymore at all.
-        But that's the point of all this: to preserve a few things that are
-        important to me, so I don't lose them. Some things I've seen, some
-        thoughts I've had, and some people I've been. It's my own sort of
-        Neocity. So, to wrap this all up, a message for you, for you reading
-        this, wherever and whenever you are..!`}
+        {
+          "There's a Jane Hirshfield poem I read as a kid that's always been really important to me:"
+        }
       </p>
+      <br />
+      <p className="italic">{"Little soul,"}</p>
+      <p className="italic">{"you and I will become"}</p>
+      <br />
+      <p className="italic">{"the memory"}</p>
+      <p className="italic">{"of a memory of a memory."}</p>
+      <br />
+      <p className="italic">{"A horse"}</p>
+      <p className="italic">{"released of the traces"}</p>
+      <p className="italic">{"forgets the weight of the wagon."}</p>
+      <br />
+      <p>
+        {`This site isn't meant to go on forever. Like all those Neocities, there will come a time when jonnylee.net will come crashing down. Maybe I'll have decided to take it down myself, or maybe I'll have been killed a Western shootout of some kind. So, then, the point of a website like this is to capture, over the course of however long it lasts, a bunch of different versions of "me"– to capture all these little souls. `}
+      </p>
+      <br />
+      <p>{`So, then, whether you're a future version of me looking back at this, or a good friend of mine, whether the last time we talked was yesterday or the week before or years before that:`}</p>
       <br />
       <p>Hey, here I am! Here this is!</p>
-      {/* But that's not really the point in the first place. That was my
-      long-winded way of saying that that's the exact reason I made this
-      website. Soon I'll die. , And behind every single one is someone saying:
-      "" And so clicking into a new Neocity fills me with the variety of feeling
-      I get when I step into a room I haven't been inside before. Sometimes it's
-      awful; there's mold creeping up the walls, or the pervading sense that no
-      one else has been inside for years. Or sometimes it's too small, leaves
-      you feeling trapped in. Or sometimes, rarely, if things are just right, it
-      can feel like home. But whatever any of these Neocities make me feel, I
-      love pretty much every single one. Because behind I think to some extent
-      they're a reaction, in part, to the chaos of the modern world. Some
-      Neocities even have message boards where you can drop a note for the next
-      person to see. Each one is a place where the creator said: "Hey, here I
-      am! Here this is!" I think it's a remarkably human thing. There's been a
-      lot said already about the I think there's something to be said also about
-      I guess my creating this site is my own small reaction to that. on the
-      internet where you can go, a place where you can stop and take a little
-      bit of a rest. they're places where in the same way that there used to be
-      taverns out in the wilderness, or oases in the unexplored desert. This
-      place, like all other places, will probably go away one day. I'm paying
-      about 11 bucks a year for the domain, and who knows where it'll go once
-      it's gone. This is a place where I'm going to keep things I don't want to
-      lose– it's important to hold onto stuff like that, I think.
-      <p>
-        I'll share this site w/ a few good friends. If you are here, you are one
-        of them. But mostly this place is for you! Aka the future me. So every
-        little bit of everything in here is going to be somet
-      </p> */}
     </ContentPageWrapper>
   );
 }
