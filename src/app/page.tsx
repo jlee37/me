e5709df4fc78d09ffa72a064173501eb56ec46d0
jlee37 <jonathanslee4@gmail.com>
2025-03-24 13:27:20 -0700

    try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@
 
 import GifCycler from "@/components/GifCycler";
 import Sidebar from "@/components/Sidebar";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
   return (
     <div className="pt-12 w-full h-full px-4 md:pr-20">
       <GifCycler />
       <div className="md:hidden mt-8">
-        <Sidebar />
+        <Sidebar key={usePathname()} />
       </div>
     </div>
   );
