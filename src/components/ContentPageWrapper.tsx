5d2c3940b9ecfd7b2119aa4c4c3ff8ef9bb6199e
jlee37 <jonathanslee4@gmail.com>
2025-03-23 22:58:30 -0700

    ok

--- src/components/ContentPageWrapper.tsx
+++ src/components/ContentPageWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const ContentPageWrapper = (props: { children: ReactNode }) => {
   return (
-    <div className="h-full pt-6 pb-8 md:pb-32 px-4 md:px-12 md:mr-20">
+    <div className="h-full pt-6 pb-8 px-4 md:px-12 md:mr-20">
       <div className="border border-white p-[2px] rounded-lg h-full">
         <div className="border border-white rounded-lg p-6 md:p-12 mx-auto h-full overflow-y-auto">
           {props.children}
