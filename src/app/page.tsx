021660fd7b6af0539c6e2f6bd51381d846840146
jlee37 <jonathanslee4@gmail.com>
2025-03-25 16:03:44 -0400

    try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@
 import GifCycler from "@/components/GifCycler";
 import Sidebar from "@/components/Sidebar";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const a = usePathname();
+
   return (
     <div className="pt-12 w-full h-full px-4 md:pr-20">
       <GifCycler />
+      {a}
       <div className="md:hidden mt-8">
         <Sidebar key={usePathname()} />
       </div>
