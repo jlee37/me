14f6e862ec1da94da4bb3a3988dcfec207742842
jlee37 <jonathanslee4@gmail.com>
2025-07-08 19:41:29 -0400

    fix

--- src/components/KeyButtonWithSuspense.tsx
+++ src/components/KeyButtonWithSuspense.tsx
@@ -2,10 +2,10 @@
 import { Suspense } from "react";
 import KeyButton from "./KeyButton";
 
-export default function KeyButtonWithSuspense(props: any) {
+export default function KeyButtonWithSuspense() {
   return (
     <Suspense fallback={null}>
-      <KeyButton {...props} />
+      <KeyButton />
     </Suspense>
   );
 }
