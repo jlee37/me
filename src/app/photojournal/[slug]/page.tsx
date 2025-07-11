9c9e73bcfc70840dcc7ae89948b9dc3f15aa9d31
jlee37 <jonathanslee4@gmail.com>
2025-07-11 19:25:03 -0400

    try

--- src/app/photojournal/[slug]/page.tsx
+++ src/app/photojournal/[slug]/page.tsx
@@ -48,7 +48,7 @@ export async function generateMetadata({
   if (previewPhoto?.fields?.file?.url) {
     const baseUrl = previewPhoto.fields.file.url as string;
     const cleanUrl = baseUrl.startsWith("//") ? `https:${baseUrl}` : baseUrl;
-    imageUrl = `${cleanUrl}?w=800&h=420&fit=thumb&fm=jpg&q=25`;
+    imageUrl = `${cleanUrl}?w=800&h=420&fit=thumb&fm=jpg&q=10`;
   } else {
     if (memory?.fields?.photos) {
       const photos = memory.fields.photos as Asset[];
