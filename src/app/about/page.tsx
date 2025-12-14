b25d02e69b1134017a9ca85f749c45dad3362dd8
jlee37 <jonathanslee4@gmail.com>
2025-12-13 18:41:39 -0800

    Ok

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -38,17 +38,17 @@ export default function AboutPage() {
         </Link>
       </div>
       <p>
-        {`Each one's a hyper-stylized little spot in the internet, a little oasis of self-expression. They're small pockets of community; many of them even feature message boards.
+        {`So each one's a hyper-stylized internet pitstop, a little oasis of self-expression.
   
-        Regardless of what the Neocity looks like, though, whether there's flashing gifs everywhere and not a name in sight, or a full bio and a list of all the books and songs the person's ever wanted to love, the same sentiment runs behind every
+        Regardless of what the Neocity looks like, though, whether there's flashing gifs everywhere and not a name in sight, or a full bio and a whole message board, the same base sentiment runs behind every
         single site– behind every single one, there's someone, someone out there in the hugeness of this world, saying: "Here I am!
         Here this is!"`}
       </p>
       <br />
       <p>
-        {`What used to strike me as sad, though, is how a lot of these people talk about how Neocities are special because they're permanent, permanent in a way that a journal
-        isn't, that the human body isn't. It's sad because it's
-        not really true. There's no guarantee that the people running the Neocities service will still want to do it a year from now, five years from now. Sooner or later they'll get tired of it, or they'll die. So really there's no way, no how: Neocities
+        {`What strikes me as slightly sad, though, is how a lot of these people write about how their Neocity is special because it's permanent, permanent in the way that a journal
+        isn't, in the way that the human body isn't. It's sad because it's
+        not really true. There's no guarantee that the people behind the Neocities service will still be running it a year from now, five years from now. Sooner or later they'll get tired of it, or they'll die, or it'll just get too plain expensive. So really there's no way, no how: Neocities
         will end. A million sites, a million custom cursors, a million heartfelt
         self-introductions will one day liquify into error 404 slush. So one wonders: is this all for
         nothing! Is this how every Neocity will be remembered, as amusement parks ditched and left behind? 
@@ -79,7 +79,7 @@ export default function AboutPage() {
       <p className="italic">{"forgets the weight of the wagon."}</p>
       <br />
       <p>
-        {`This site isn't meant to go on forever. Like all those Neocities, like all those traces released, there will come a time when jonnylee.net will vanish. Maybe I'll have decided to take it down myself, or maybe I'll have been killed in a Western shootout of some kind. So, then, the point of this website to capture, over the course of however long it lasts, a bunch of different versions of "me"– to capture all these little souls. And so whether you're a future version of me looking back at this, or a good friend of mine, whether the last time we talked was yesterday or the week before or years before that:`}
+        {`This site isn't meant to go on forever. Like all those Neocities, like all those traces released, there will come a time when jonnylee.net will vanish. Maybe I'll have decided to take it down myself, or maybe I'll have been killed in a Western shootout of some kind. So, then, the purpose of this website to capture, over the course of however long it lasts, a bunch of different versions of "me"– to capture all these little souls. And so whether you're a future version of me looking back at this, or a good friend of mine, whether the last time we talked was yesterday or the week before or years before that:`}
       </p>
       <br />
       <p>Here I am! Here this is!</p>
